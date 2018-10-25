d38204f44f85fe51473d6836dee35bf8e5226a24
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2018-10-24 19:51:36 -0500

    Change new path name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :actors
+  resources :actors, only: [:index, :create]
+  get 'actors/new' => 'actors#new', as: 'new_actors'
 
   get 'welcome/index'
 end
